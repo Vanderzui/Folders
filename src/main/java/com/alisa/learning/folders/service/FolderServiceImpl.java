12f2f9ab5533818bff304a2c0a534fea277fd400
Vanderzui <zu.alisa@gmail.com>
2022-08-03 19:13:48 +0300

    feature/f-02: First implementation: just comparing names of files and directories inside particularly folders

--- src/main/java/com/alisa/learning/folders/service/FolderServiceImpl.java
+++ src/main/java/com/alisa/learning/folders/service/FolderServiceImpl.java
@@ -1,4 +1,4 @@
-package com.alisa.learning.folders;
+package com.alisa.learning.folders.service;
 
 import java.io.File;
 import java.io.IOException;
@@ -6,31 +6,77 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
 public class FolderServiceImpl implements FolderService {
 
+    private static final String DIRECTORIES = "directories";
+    private static final String FILES = "files";
+    private Map<String, List<String>> mapFirst;
+    private Map<String, List<String>> mapSecond;
+
+    public FolderServiceImpl() {
+        mapFirst = new LinkedHashMap<>();
+        mapFirst.put(DIRECTORIES, new ArrayList<>());
+        mapFirst.put(FILES, new ArrayList<>());
+
+        mapSecond = new LinkedHashMap<>();
+        mapSecond.put(DIRECTORIES, new ArrayList<>());
+        mapSecond.put(FILES, new ArrayList<>());
+    }
+
+
     @Override
-    public void loadFile() throws IOException {
+    public void compareFileSystems() throws IOException {
         Properties prop = new Properties();
         InputStream is = getClass().getClassLoader().getResourceAsStream("application.properties");
         prop.load(is);
+
         final var firstPath = prop.getProperty("directory.first");
         final var secondPath = prop.getProperty("directory.second");
 
         File first = new File(firstPath);
         File second = new File(secondPath);
-//        file.exists();
-//        file.getName();
-//        Arrays.stream(file.listFiles()).map(f -> f.getName()).toList(); //f ->folderPath + "/" + f.getName()
-        System.out.println("");
-        compareDirectoriesByFileNamesInside(firstPath, secondPath);
 
+        flattenTheTree(first, mapFirst);
+        flattenTheTree(second, mapSecond);
+
+        String equal = mapFirst.equals(mapSecond) ? "are equal" : " are not equal";
+        System.out.println(
+                "Two different directories one = %s and another %s with some stuff in it %s"
+                        .formatted(firstPath, secondPath, equal));
 
     }
 
+    private void flattenTheTree(File file, Map<String, List<String>> targetMap) {
+        var files = file.listFiles();
+        for (File innerFile : files) {
+            addToMap(innerFile, targetMap);
+            if (innerFile.isDirectory()) {
+                flattenTheTree(innerFile, targetMap);
+            }
+        }
+    }
+
+    private void addToMap(File file, Map<String, List<String>> targetMap) {
+        if (file.isDirectory()) {
+            var dirs = targetMap.get(DIRECTORIES);
+            dirs.add(file.getName());
+        } else if (file.isFile()) {
+            var files = targetMap.get(FILES);
+            files.add(file.getName());
+        }
+    }
+
+    /**
+     * Will use it later
+     */
     @Override
     public boolean compareTextFiles(String pathOne, String pathTwo) throws IOException {
 //        Path filePath1 = Paths.get(pathOne);
@@ -46,6 +92,9 @@ public class FolderServiceImpl implements FolderService {
 //        return content1.equals(content3));
     }
 
+    /**
+     * Will use it later or maybe not
+     */
     @Override
     public boolean compareDirectoriesByFileNamesInside(String firstDirectory,
             String secondDirectory) {
@@ -64,6 +113,7 @@ public class FolderServiceImpl implements FolderService {
     }
 }
 
+//DO NOT LOOK
 //        //old way
 //        //------------------------------------------
 //        File file = new File(folderPath);
