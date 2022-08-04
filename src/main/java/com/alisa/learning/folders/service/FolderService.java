f05b00b869a729194b653b9980cf258b44e3b02d
Vanderzui <zu.alisa@gmail.com>
2022-08-04 17:54:11 +0300

    feature/f-03: Added recursion comparing + refactoring

--- src/main/java/com/alisa/learning/folders/service/FolderService.java
+++ src/main/java/com/alisa/learning/folders/service/FolderService.java
@@ -1,6 +1,5 @@
 package com.alisa.learning.folders.service;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -8,12 +7,10 @@ public interface FolderService {
 
     void compareFileSystems() throws IOException, URISyntaxException;
 
-    boolean isEqualFiles(File pathOne, File pathTwo) throws IOException;
-
-    boolean compareDirectoriesByFileNamesInside(String firstDirectory, String secondDirectory);
-
     boolean compareUsingWalk(String first, String second) throws IOException;
 
-    void compareByNamesUsingMap(String first, String second) throws IOException;
+    boolean compareByNamesUsingMap(String first, String second) throws IOException;
+
+    boolean compareUsingRecursion(String first, String second) throws IOException;
 
 }
